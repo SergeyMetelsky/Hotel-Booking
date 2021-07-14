734d5caa95a89edd52d6e807971ff22f4e5b2357
Metelsky <n_grey@mail.ru>
2021-07-14 19:37:19 +0300

    Added manual parser for Hotel

---
+++ Hotel Booking/Models/Details.swift
@@ -0,0 +1,8 @@
+//
+//  Details.swift
+//  Hotel Booking
+//
+//  Created by Sergey on 7/13/21.
+//
+
+import Foundation
---
+++ Hotel Booking/NetworkManager/NetworkManagerDetails.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManagerDetails.swift
+//  Hotel Booking
+//
+//  Created by Sergey on 7/13/21.
+//
+
+import Foundation
